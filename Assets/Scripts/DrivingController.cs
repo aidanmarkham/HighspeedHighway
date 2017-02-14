32b710a48c93524b17b0095c522f1dde8a52ced8
Aidan Markham <apm4337@g.rit.edu>
2017-02-13 20:17:26 -0500

    Protyped movement

---
+++ Assets/Scripts/DrivingController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrivingController : MonoBehaviour {
+    public float baseSpeed;
+    public float increaseRate;
+    private float timeSoFar;
+    public float steeringForce;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeSoFar += Time.deltaTime;
+
+
+        Vector3 localVel = new Vector3();
+        localVel.z = baseSpeed + (timeSoFar * increaseRate);
+        GetComponent<Rigidbody>().velocity = transform.TransformDirection(localVel);
+
+        transform.Rotate(new Vector3(0, steeringForce * Input.GetAxis("Horizontal"), 0));
+
+    }
+}
---
+++ Assets/Scripts/SteeringReactions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringReactions : MonoBehaviour {
+    public GameObject driverCam;
+    public GameObject steeringWheel;
+
+    public float degreesToTurnWheel;
+    public float degreesToTurnCamera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        steeringWheel.transform.localEulerAngles = new Vector3(0, degreesToTurnWheel * Input.GetAxis("Horizontal"), 0);
+        driverCam.transform.localEulerAngles = new Vector3(0, degreesToTurnCamera * Input.GetAxis("Horizontal"), 0);
+
+    }
+}
